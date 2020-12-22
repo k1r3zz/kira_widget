14d2ed74528c84343f5df33f82d287a3c13d957b
kira <644904088@qq.com>
2020-12-22 11:47:57 +0800

    kira widget

--- lib/kira_container.dart
+++ lib/kira_container.dart
@@ -13,6 +13,8 @@ class KContainer extends StatelessWidget {
     this.decoration,
     this.border,
     this.child,
+    this.backgroundAsset,
+    this.backgroundNet,
     this.color = const Color(0xffffffff),
     this.alignment = Alignment.center,
     Key key,
@@ -27,26 +29,47 @@ class KContainer extends StatelessWidget {
   final double height;
   final Alignment alignment;
   final Widget child;
+  final String backgroundAsset;
+  final String backgroundNet;
   final double radius;
   final BoxBorder border;
   Decoration decoration;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-          left: padding,
-          top: topPadding == 0 ? padding : topPadding,
-          right: rightPadding == 0 ? padding : rightPadding,
-          bottom: buttompadding == 0 ? padding : buttompadding),
-      width: width,
-      height: height,
-      alignment: alignment,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(radius),
-          color: color,
-          border: border),
-      child: child,
+    return Stack(
+      children: [
+        backgroundAsset != null || backgroundNet != null
+            ? Positioned.fill(
+                child: backgroundAsset != null
+                    ? Image.asset(
+                        backgroundAsset,
+                        fit: BoxFit.fill,
+                      )
+                    : backgroundNet != null
+                        ? Image.network(
+                            backgroundNet,
+                            fit: BoxFit.fill,
+                          )
+                        : Container(),
+              )
+            : Container(),
+        Container(
+          padding: EdgeInsets.only(
+              left: padding,
+              top: topPadding == 0 ? padding : topPadding,
+              right: rightPadding == 0 ? padding : rightPadding,
+              bottom: buttompadding == 0 ? padding : buttompadding),
+          width: width,
+          height: height,
+          alignment: alignment,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(radius),
+              color: color,
+              border: border),
+          child: child,
+        ),
+      ],
     );
   }
 }
