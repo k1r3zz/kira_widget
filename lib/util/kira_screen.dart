02e13e45bf2670857937a61bcf54bd8729185180
kira <644904088@qq.com>
2021-01-08 16:34:10 +0800

    kira screen

--- lib/util/kira_screen.dart
+++ lib/util/kira_screen.dart
@@ -3,13 +3,15 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 ///屏幕宽度
-double screenWidth = MediaQueryData.fromWindow(window).size.width;
+class KScreen {
+  static double screenWidth = MediaQueryData.fromWindow(window).size.width;
 
-///屏幕高度
-double screenHeight = MediaQueryData.fromWindow(window).size.height;
+  ///屏幕高度
+  static double screenHeight = MediaQueryData.fromWindow(window).size.height;
 
-///状态栏高度
-double statusHeight = MediaQueryData.fromWindow(window).padding.top;
+  ///状态栏高度
+  static double statusHeight = MediaQueryData.fromWindow(window).padding.top;
 
-///导航栏高度
-const double appBarHeight=kMinInteractiveDimension;
+  ///导航栏高度
+  static double appBarHeight = kMinInteractiveDimension;
+}
