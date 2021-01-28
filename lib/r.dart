865c8dbdd6e4d94ca6a6ed0f6c7c8b7d3ccec1ac
kira <644904088@qq.com>
2021-01-28 17:10:08 +0800

    kira 加入KCoverWidget ,加入KWidgetRun

---
+++ lib/r.dart
@@ -0,0 +1,12 @@
+class R {
+  /// ![](http://127.0.0.1:9555/assets/images/image_error.png)
+  static final String assetsImagesImageError = 'assets/images/image_error.png';
+  /// ![](http://127.0.0.1:9555/assets/images/image_loading.png)
+  static final String assetsImagesImageLoading = 'assets/images/image_loading.png';
+  /// ![](http://127.0.0.1:9555/assets/images/icon_cover_up.png)
+  static final String assetsImagesIconCoverUp = 'assets/images/icon_cover_up.png';
+  /// ![](http://127.0.0.1:9555/assets/images/icon_cover_down.png)
+  static final String assetsImagesIconCoverDown = 'assets/images/icon_cover_down.png';
+  /// ![](http://127.0.0.1:9555/assets/images/arrow_down.png)
+  static final String assetsImagesArrowDown = 'assets/images/arrow_down.png';
+}
